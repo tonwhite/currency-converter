de6c85bb1dcc67e4d893733a20efa96cf92339d7
Anton Glushchenko <anton.glushchenko@gmail.com>
2023-07-25 18:07:24 +0300

    completed coverter

---
+++ index.js
@@ -0,0 +1,58 @@
+const API_KEY = "09536f52a5e5ea14d81524f6";
+
+const fromCur = document.querySelector(".from select");
+const toCur = document.querySelector(".to select");
+const getBtn = document.querySelector("form button");
+const exIcon = document.querySelector("form .reverse");
+const amount = document.querySelector("form input");
+const exRateTxt = document.querySelector("form .result");
+
+// Слухач подій для випадаючого списку валют (select)
+
+[fromCur, toCur].forEach((select, i) => {
+    for (let curCode in Country_List) {
+        const selected = (i === 0 && curCode === "USD") || (i === 1 && curCode === "GBP") ? "selected" : "";
+        select.insertAdjacentHTML("beforeend", `<option value="${curCode}" ${selected}>${curCode}</option>`);
+    }
+    select.addEventListener("change", () => {
+        const code = select.value;
+        const imgTag = select.parentElement.querySelector("img");
+        imgTag.src = `https://flagcdn.com/48x36/${Country_List[code].toLowerCase()}.png`;
+    });
+});
+
+// Функція для отримання курсу обміну валюти з API
+
+async function getExchangeRate() {
+    const amountVal = amount.value || 1;
+    exRateTxt.innerText = "Getting exchange rate...";
+    try {
+        const response = await fetch(`https://v6.exchangerate-api.com/v6/${API_KEY}/latest/${fromCur.value}`);
+        if (!response.ok) throw new Error('Network response was not ok');
+        const result = await response.json();
+        const exchangeRate = result.conversion_rates[toCur.value];
+        const totalExRate = (amountVal * exchangeRate).toFixed(2);
+        exRateTxt.innerText = `${amountVal} ${fromCur.value} = ${totalExRate} ${toCur.value}`;
+    } catch (error) {
+        exRateTxt.innerText = "Something went wrong...";
+        console.error('Error:', error);
+    }
+}
+
+// Слухач подій для кнопки та кліку по іконці обміну
+
+window.addEventListener("load", getExchangeRate);
+getBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    getExchangeRate();
+});
+
+exIcon.addEventListener("click", () => {
+    [fromCur.value, toCur.value] = [toCur.value, fromCur.value];
+    [fromCur, toCur].forEach((select) => {
+        const code = select.value;
+        const imgTag = select.parentElement.querySelector("img");
+        imgTag.src = `https://flagcdn.com/48x36/${Country_List[code].toLowerCase()}.png`;
+    });
+    getExchangeRate();
+});
